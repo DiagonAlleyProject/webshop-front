2160cd6a361dd81e58c9ac4f0276c30c171679eb
Erika <erikatorres2294@gmail.com>
2024-04-18 17:12:59 -0300

    Se realizo pagina de login

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+
+const btnCerrarSesion = document.querySelector('.cerrar-sesion');
+btnCerrarSesion.addEventListener('click', (ev) =>{
+    // Recuperar token de localStorage.
+    const token = localStorage.getItem('token');
+    console.log(token);
+
+    // Eleminar token de localStorage.
+    const EliminarToken = localStorage.removeItem('token');
+    console.log(EliminarToken);
+
+    // Ir pagina de logueo
+    window.location.href = '../login/login.html';
+    
+    
+})
